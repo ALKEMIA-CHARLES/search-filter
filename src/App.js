05ef39c6cdc66690a7d7e2109de864eb3b8bb258
ALKEMIA-CHARLES <charlesmtawalijr@gmail.com>
2024-05-06 12:17:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useRef, useState } from "react";
+import { flushSync } from "react-dom";
 
 function App() {
+  const [items, setItems] = useState([])
+  // const [filteredItems, setFilteredItems] = useState([])
+  const [query, SetQuery] = useState("")
+  const inputRef = useRef()
+  
+  
+  const filteredItems = useMemo(() => {
+    return items.filter(item => {
+      return item.toLowerCase().includes(query.toLowerCase())
+  })
+}, [items, query])
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    const value = inputRef.current.value
+    if (value === "") return
+    setItems(prev => {
+    return [...prev, value]
+    })
+    // setFilteredItems(prev => {
+    //   return [...prev, value]
+    // })
+    inputRef.current.value = ""
+  }
+  
+  // function onChange(e) {
+  //   const value = e.target.value
+  //   setFilteredItems(items.filter(item => item.toLowerCase().includes(value.toLowerCase()))
+  //   )
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      Search
+      <input value={query} onChange={e => SetQuery(e.target.value)} type="search" />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New item: <input ref={inputRef} type="text" />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items:</h3>
+      {filteredItems.map(item => (
+        <div>{item}</div>
+      ))}
+    </>
+  )
+
 }
 
 export default App;
